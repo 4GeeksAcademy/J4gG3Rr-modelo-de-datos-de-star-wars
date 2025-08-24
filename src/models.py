2f5b5e64b6e8d4194bc39c69ca8379451fe95581
Johan Gomez <johangomez0612@gmail.com>
2025-08-24 22:45:31 +0000

    data model star wars terminado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -8,12 +8,66 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
 
+    favorites: Mapped[list["Favoritos"]] = relationship("Favoritos", back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            # do not serialize the password, its a security breach
+        }
+
+class Personajes(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    gender: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            # do not serialize the password, its a security breach
+        }
+
+class Planetas(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            # do not serialize the password, its a security breach
+        }
+
+class Favoritos(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id = mapped_column(ForeignKey('user.id'), nullable=False)
+    character_id = mapped_column(ForeignKey('personajes.id'), nullable=True)
+    planet_id = mapped_column(ForeignKey('planetas.id'), nullable=True)
+
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Personajes", back_populates="favorites")
+    planet = relationship("Planetas", back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
             # do not serialize the password, its a security breach
         }
